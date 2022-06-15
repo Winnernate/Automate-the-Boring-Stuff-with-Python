05a53125d6e55d52fd3660a5516fb123e7f430bf
Winnernate <106708463+Winnernate@users.noreply.github.com>
2022-06-15 10:06:22 -0700

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,8 @@
+name = 'Mary'
+password = 'swordfish'
+if name == 'Mary':
+    print('Hello Dad')
+    if password == 'swordfish':
+        print('Access granted.')
+    else:
+        print('Wrong password.')
